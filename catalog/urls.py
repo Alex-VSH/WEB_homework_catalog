c523412a0fae66ebabc6d843121b45e9cc8efce8
Alex_Volk-Shevalye <aisb@tut.by>
2023-11-06 15:18:33 +0300

    changes for homework #4

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from catalog.views import index_home, index_contacts
+from catalog.views import index_home, index_contacts, index_show_good
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index_home),
-    path('contacts/', index_contacts)
+    path('', index_home, name='home'),
+    path('contacts/', index_contacts, name='contacts'),
+    path('<int:pk>/show_good/', index_show_good, name='show_good')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+
+from catalog.models import Products
+
 
 # Create your views here.
 
+
+def index_base(requests):
+    return render(requests, 'catalog/base.html')
+
+
 def index_home(requests):
-    return render(requests, 'catalog/home.html')
+    goods_list = Products.objects.all()
+    context = {
+        "object_list": goods_list
+    }
+    return render(requests, 'catalog/home.html', context)
+
 
 def index_contacts(requests):
     return render(requests, 'catalog/contacts.html')
+
+
+def index_show_good(requests, pk):
+    context = {
+        "object": Products.objects.get(pk=pk)
+    }
+    return render(requests, 'catalog/show_good.html', context)
